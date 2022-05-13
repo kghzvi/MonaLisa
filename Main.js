480e9025b2967ffd4fcd51ee6d527ba37c24e7ed
Halders <halderst@gmx.de>
2022-05-13 21:34:06 +0200

    added touchmove

--- Main.js
+++ Main.js
@@ -23,3 +23,25 @@ window.addEventListener('mousemove', e => {
 
     
 });
+
+window.addEventListener('touchmove', e => {
+    cursorX = e.clientX;    
+    cursorY = e.clientY;    
+    x = cursorX - 272;
+    y = -1 * (cursorY - 210);
+    var phi = Math.atan(y/x);
+    if (x<0) {phi = phi + Math.PI}
+    if (x>0 && y<0) {phi = phi + 2*Math.PI}    
+    eye1.style.left = 30 * Math.cos(phi) + 230;
+    eye1.style.top = -60 * Math.sin(phi) + 150;
+
+    x = cursorX - 448;
+    y = -1 * (cursorY - 210);
+    phi = Math.atan(y/x);
+    if (x<0) {phi = phi + Math.PI}
+    if (x>0 && y<0) {phi = phi + 2*Math.PI}    
+    eye2.style.left = 30 * Math.cos(phi) + 400;
+    eye2.style.top = -60 * Math.sin(phi) + 150;
+
+    
+});
